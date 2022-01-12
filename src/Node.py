447ced37108593049c418fdd6c052119ea2423c1
Plancke Aurelien <plancke@a11p7.fil.univ-lille1.fr>
2022-01-12 11:45:11 +0100

    IN PROGRESS:Node

--- src/Node.py
+++ src/Node.py
@@ -1,7 +1,14 @@
 class Node():
 
-    def __init__(self, keys, children, parent):
+    def __init__(self, keys, children, parent, isLeaf):
         self.nbKeys = len(keys)
         self.keys = keys
         self.children = children
         self.parent = parent
+        self.isLeaf = False
+
+    def setIsLeaf(self, isLeaf):
+        self.isLeaf = isLeaf
+    
+    def getChildrens(self):
+        return self.children
---
+++ src/main.py
@@ -0,0 +1,10 @@
+from Btree.py import BTree
+
+class main():
+
+    def main(self):
+        root = new Node()
+        tree = new BTree()
+
+    if __name__ == "__main__":
+        main()
